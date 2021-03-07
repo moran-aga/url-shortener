bdbbd85d556f1a018215b60b54b886cc7e302264
moran-aga <77380388+moran-aga@users.noreply.github.com>
2021-03-07 09:34:39 +0200

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const app = require("./app");
-// const router = require("./statistic");
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
- console.log(`Listening on port ${PORT}`);
-});
